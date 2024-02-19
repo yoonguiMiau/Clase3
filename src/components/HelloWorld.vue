c902c4c4f5cca3b38491708dffcb88adc9ae6347
yoonguiMiau <kariaguirre2005@gmail.com>
2024-02-19 11:41:08 -0600

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1> Informacion Per: {{ info }} </h1>
+    <img :src="require('../assets/img/megumi.jpg')" alt="megumi" style="width: 200px; height: auto;">
   </div>
 </template>
 
@@ -8,7 +9,7 @@
 export default {
   data () {
     return {
-      info: ['Karina','20961','5A']
+      info: 'Karina,20961,5A'
     }
   }
 }
